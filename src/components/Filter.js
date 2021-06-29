3cc15bc910f71125b5caa4554e692363db6b85ff
Vandana <yadavvk2010@gmail.com>
2021-06-29 22:27:44 +0530

    add redux to filter component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,33 +1,46 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { sortProducts, filterProducts } from "../actions/productActions";
 
-export default class Filter extends Component {
+class Filter extends Component {
     render() {
         return (
-            <div className="filter">
-                <div className="filter-result">
-                    {this.props.count} Products
+            !this.props.filteredProducts ? (
+                <div>Loading...</div>
+            ) : (
+                <div className="filter">
+                    <div className="filter-result">
+                        {this.props.filteredProducts.length} Products
+                    </div>
+                    <div className="filter-sort">
+                        Order{" "}
+                        <select value={this.props.sort} onChange={(e) => this.props.sortProducts(this.props.filteredProducts, e.target.value)}>
+                            <option value="latest">Latest</option>
+                            <option value="lowest">Lowest</option>
+                            <option value="highest">Highest</option>
+                        </select>
+                    </div>
+                    <div className="filter-size">
+                        Filter{" "}
+                        <select value={this.props.size} onChange={(e) => this.props.filterProducts(this.props.products, e.target.value)}>
+                            <option Value="">ALL</option>
+                            <option Value="XS">XS</option>
+                            <option Value="S">S</option>
+                            <option Value="M">M</option>
+                            <option Value="L">L</option>
+                            <option Value="XL">XL</option>
+                            <option Value="XXL">XXL</option>
+                        </select>
+                    </div>
                 </div>
-                <div className="filter-sort">
-                    Order{" "}
-                    <select value={this.props.sort} onChange={this.props.sortProducts}>
-                        <option>Latest</option>
-                        <option value="lowest">Lowest</option>
-                        <option value="highest">Highest</option>
-                    </select>
-                </div>
-                <div className="filter-size">
-                    Filter{" "}
-                    <select value={this.props.size} onChange={this.props.filterProducts}>
-                        <option Value="">ALL</option>
-                        <option Value="XS">XS</option>
-                        <option Value="S">S</option>
-                        <option Value="M">M</option>
-                        <option Value="L">L</option>
-                        <option Value="XL">XL</option>
-                        <option Value="XXL">XXL</option>
-                    </select>
-                </div>
-            </div>
+            )
         )
     }
 }
+
+export default connect((state) => ({
+    size: state.products.size,
+    sort: state.products.sort,
+    products: state.products.items,
+    filteredProducts: state.products.filteredItems
+}), { filterProducts, sortProducts })(Filter)
--- src/components/Products.js
+++ src/components/Products.js
@@ -97,4 +97,4 @@ class Products extends Component {
         )
     }
 }
-export default connect((state) => ({ products: state.products.items }), { fetchProducts })(Products)
+export default connect((state) => ({ products: state.products.filteredItems }), { fetchProducts })(Products)
